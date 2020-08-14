ccc5b1f89014e8cf4c775f18c8a928e5d69a1de4
vilas20-10-98 <2016BPR030@sggs.ac.in>
2020-08-14 14:47:27 +0530

    added first name

--- use_reg.sh
+++ use_reg.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -x
 
 echo "welcome to use registration rpository"
+read -p "Enter the first name: " First name 
+pattern="^([[:upper:]]{1})([[:lower:]]{2,3})$"
+if [[ $Firstname =~ $pattern ]]
+then
+   echo $Firstname;
+else
+   echo "The First Letter Should be in capital";
+   exit
+
+fi
 
